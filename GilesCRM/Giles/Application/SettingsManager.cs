c60b0a4990758ec8faf294c3c24d306f6deaacc0
jesswhitworth <jessewhitworth@gmail.com>
2016-06-06 17:24:53 -0400

    Create SettingsManager.cs

---
+++ GilesCRM/Giles/Application/SettingsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GilesCRM.Giles.Application
+{
+    class SettingsManager {
+        SettingsData mySettings;
+        public SettingsManager(string pathName) {
+            FileStream stream = new FileStream(pathName, FileMode.OpenOrCreate);
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(SettingsData));
+            mySettings = (SettingsData)ser.ReadObject(stream);
+        }
+        
+        public string getDefaultView() {
+          return mySettings.defaultViewFilename;
+        }
+        
+        public ConnectionData getDefaultConnection() {
+          return mySettings.defaultConnection;
+        }
+    }
+    [DataContract}
+    public class ConnectionData {
+      [DataMember]
+      string databaseName;
+      [DataMember]
+      string userName;
+      [DataMember]
+      string password;
+    }
+    [DataContract]
+    class SettingsData {
+      [DataMember]
+      ConnectionData defaultConnection;
+      [DataMember]
+      string defaultViewFilename;
+      
+    }
+}
