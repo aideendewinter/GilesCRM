55d861c3aaf245024f2752508e5dfb3246ba95b8
jesswhitworth <jessewhitworth@gmail.com>
2016-06-08 16:50:31 -0400

    Update SettingsManager.cs

--- GilesCRM/Giles/App/SettingsManager.cs
+++ GilesCRM/Giles/App/SettingsManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GilesCRM.Giles.Application
+namespace GilesCRM.Giles.App
 {
     class SettingsManager {
         SettingsData mySettings;
