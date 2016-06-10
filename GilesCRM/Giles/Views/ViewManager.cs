aa585c21c1093d529294393b72a33fe598abca22
jesswhitworth <jessewhitworth@gmail.com>
2016-06-10 16:25:18 -0400

    Update ViewManager.cs

--- GilesCRM/Giles/Views/ViewManager.cs
+++ GilesCRM/Giles/Views/ViewManager.cs
@@ -10,18 +10,19 @@ namespace GilesCRM.Giles.Views
     class ViewManager
     {
         BaseView currentView;
-        public ViewManager(SettingsManager settings) {
+        public ViewManager(SettingsManager settings, ConnectionManager connections) {
             string defaultViewFilename = settings.getDefaultView();
             // TO-DO: Error handling.
             FileStream stream = new FileStream(defaultViewFilename, FileMode.Open);
             DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(BaseView));
             currentView = ser.ReadObject(stream) as BaseView;
-            UpdateView(settings);
+            UpdateView(settings, connections.getDefaultConnection());
         }
         
-        private void UpdateView(SettingsManager settings)
+        private void UpdateView(SettingsManager settings, NpgsqlConnection connection)
         {
-            ((ContentControl)Application.Current.MainWindow.FindName("ViewContent")).Content = currentView.GetUI(settings);
+            ((ContentControl)Application.Current.MainWindow.FindName("ViewContent")).Content
+                = currentView.GetUI(settings, connection);
         }
     }
 }
