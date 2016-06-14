25279db7e99fe7e5ac3808783c3f0dfb3c885df7
jesswhitworth <jessewhitworth@gmail.com>
2016-06-14 13:45:41 -0400

    Layout work

---
+++ GilesCRM/Placeholder.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GilesCRM
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class Placeholder : Window
+    {
+        public Placeholder()
+        {
+            InitializeComponent();
+        }
+    }
+}
